41655a9a9ba142d9ec63c88202ecf1673173899b
David Griffiths <davidgg777@hotmail.com>
2021-05-03 15:53:15 +1000

    cleaned up dbt DAGs and docs

--- dags/dbt_selectors_standard_schedule.py
+++ dags/dbt_selectors_standard_schedule.py
@@ -14,14 +14,10 @@ from airflow.utils.dates import timedelta
 default_dag_args = {
     "start_date": datetime(2020, 11, 24),
     "retry_delay": timedelta(minutes=10),
-    # "on_failure_callback": notify_all_of_failure,
     "depends_on_past": False,
     "retries": 0,
 }
 DAG_NAME = "standard_schedule"
-# DBT_DIR = "/home/dave/data-engineering/data-cicd"
-# DBT_DIR = "/usr/local/airflow/dags/dbt"
-# DBT_SELECTOR_PICKLE_DIR = "/home/dave/data-engineering/data-cicd/dbt_dags/data"
 DBT_DIR = "/usr/local/airflow/data-cicd"
 DBT_SELECTOR_PICKLE_DIR = "/usr/local/airflow/include/data"
 GLOBAL_CLI_FLAGS = "--no-write-json"
@@ -52,11 +48,9 @@ def load_dag_def_pickle(file: str):
     return objects[0]
 
 
-##TODO - how to get dbt dir into airflow to run? i.e. where is DBT_DIR
-
-
 def load_manifest():
-    data_folder = Path(DBT_SELECTOR_PICKLE_DIR, "manifest.json")
+    """loads the dbt manifest metadata file"""
+    data_folder = Path(f"{DBT_DIR}", "target", "manifest.json")
     with open(data_folder) as f:
         data = json.load(f)
     return data
